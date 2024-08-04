70d9a0e0e1baec05af88b6b820dabc66ed68be56
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2024-08-04 21:12:27 +0630

    init

---
+++ chapter2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main(String[]args)
+    {
+        int a = 3;
+        int b = 2;
+        System.out.println(a/b);
+    }
+}
---
+++ chapter3/test-window.js
@@ -0,0 +1,1 @@
+console.log('No window ',global);
